3005428f584e9623efc97d2707814eb3bda3b337
yilmazbatuengin <sns6267f9s@privaterelay.appleid.com>
2026-04-29 20:13:25 +0200

    finished exercise 02 - scores average

--- github-education-c-exercises/02-scores-average/scores.c
+++ github-education-c-exercises/02-scores-average/scores.c
@@ -1,25 +1,30 @@
-#include <cs50.h>
 #include <stdio.h>
 
-// TODO: change N if you want more inputs
 const int N = 3;
 
-// TODO: implement this function
 float average(int length, int array[]);
 
 int main(void)
 {
     int scores[N];
 
-    // TODO: ask user for N scores and store them in scores[]
-    // Example prompt: "Score: "
+    for (int i = 0; i < N; i++)
+    {
+        printf("Score: ");
+        scanf("%d", &scores[i]);
+    }
 
-    // TODO: print average
-    // Example: printf("Average: %.2f\n", average(N, scores));
+    printf("Average: %.2f\n", average(N, scores));
 }
 
 float average(int length, int array[])
 {
-    // TODO: compute and return average
-    return 0.0;
+    int sum = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+
+    return (float) sum / length;
 }
