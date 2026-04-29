f8570e679f4197536af4ea2b03641c539f136878
yilmazbatuengin <sns6267f9s@privaterelay.appleid.com>
2026-04-29 14:14:31 +0200

    Update buggy4.c

--- github-education-c-exercises/01-debugging/buggy4.c
+++ github-education-c-exercises/01-debugging/buggy4.c
@@ -1,9 +1,8 @@
-// Buggy example for printf
 #include <stdio.h>
 
 int main(void)
 {
-    for (int i = 0; i <= 3; i++)
+    for (int i = 0; i < 4; i++)
     {
         printf("i is %i\n", i);
         printf("#\n");
